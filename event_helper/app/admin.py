9f74fcba398c8cf436121ab37ce86a666641a693
KIm, Hyeonseo <dodok8@gmail.com>
2019-05-11 23:24:22 +0900

    Make usermodel

--- event_helper/app/admin.py
+++ event_helper/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Participant
+
+admin.site.register(Participant, UserAdmin)
 
 # Register your models here.
--- event_helper/app/views.py
+++ event_helper/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from library import *
+from .library import qr_function, user_function, gps_function, translate_function
 
 def test_view(request):
     return JsonResponse({'status':'200', 'code':'only for test'})
