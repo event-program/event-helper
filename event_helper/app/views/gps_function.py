b7dfbf16eb6546b41363f8af9a3dee6e21bb004b
KIm, Hyeonseo <dodok8@gmail.com>
2019-05-12 04:36:36 +0900

    Update gps_function

--- event_helper/app/views/gps_function.py
+++ event_helper/app/views/gps_function.py
@@ -1,7 +1,7 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404
 from ..models import Participant, Event
-import qr_function
+from . import qr_view
 
 def gps_view(request):
     # return Dict.
@@ -12,6 +12,7 @@ def gps_view(request):
         user_id = request.POST.get('user_id','')
 
         now_event = get_object_or_404(Event, name="정션X")
+        now_user = get_object_or_404(Participant, user_id=user_id)
         # 이벤트 1개 임시
         event_la, event_lo = now_event.location.split(' ')
 
@@ -19,7 +20,7 @@ def gps_view(request):
             if (abs(float(event_lo) - float(longitude)) > 0.0001) or (abs(float(event_la) - float(latitude)) > 0.0001) :
                 return JsonResponse({'status_code':'404','qr_image':''})
 
-        return qr_function.qr_view(request)
+        return qr_view(request)
 
     else:
-        return 404
+        return HttpResponseNotFound
