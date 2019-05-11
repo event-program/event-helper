a5f035daebfe3b4d71d594e84767855b6b0a7600
KIm, Hyeonseo <dodok8@gmail.com>
2019-05-11 21:57:56 +0900

    Have some error

--- event_helper/app/models.py
+++ event_helper/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class Event(models.Model):
+    pass
+
+class Alert(models.Model):
+    noti_type = models.CharField(max_length=30)
+    time = models.DateTimeField()
+    location = models.CharField(max_length=30)
+    description = models.TextField()
+
 # Create your models here.
