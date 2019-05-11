b53edeb566ad4a4cc2809ffb425b366203043dc5
KIm, Hyeonseo <dodok8@gmail.com>
2019-05-12 00:49:09 +0900

    Edited parameter of CharField

--- event_helper/app/models.py
+++ event_helper/app/models.py
@@ -13,45 +13,49 @@ class Alert(models.Model):
 
 
 class Alert(models.Model):
-    type = models.CharField(max_length='2')
-    name = models.CharField(max_length='30')
+    type = models.CharField(max_length=2)
+    name = models.CharField(max_length=30)
     time = models.DateTimeField()
-    location = models.CharField(max_length='30')
+    location = models.CharField(max_length=30)
     description = models.TextField()
 
 
 class Participant(models.Model):
-    id = models.CharField(max_length='30',blank=True)
+    id = models.CharField(max_length=60,blank=True)
 
     USERNAME_FIELD = 'id'
 
-    username = models.CharField(max_length='30', blank=True)
-    phone = models.CharField(max_length='30', blank=True)
-    language = models.CharField(max_length='30', blank=True)
-    country = models.CharField(max_length='30', blank=True)
-    question_5 = models.CharField(max_length='30', blank=True)
-    question_6 = models.CharField(max_length='30', blank=True)
-    question_7 = models.CharField(max_length='30', blank=True)
-    question_8 = models.CharField(max_length='30', blank=True)
-    question_9 = models.CharField(max_length='30', blank=True)
+    username = models.CharField(max_length=30, blank=True)
+    phone = models.CharField(max_length=30, blank=True)
+    language = models.CharField(max_length=30, blank=True)
+    country = models.CharField(max_length=30, blank=True)
+    question_5 = models.CharField(max_length=30, blank=True)
+    question_6 = models.CharField(max_length=30, blank=True)
+    question_7 = models.CharField(max_length=30, blank=True)
+    question_8 = models.CharField(max_length=30, blank=True)
+    question_9 = models.CharField(max_length=30, blank=True)
     qr_code = models.TextField()
     is_staff = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.id
 
     def save(self):
         self.id = self.username + self.phone
         super(Participant, self).save()
 
+
 class Event(models.Model):
     entry = models.ManyToManyField(Participant)
-    name = models.CharField(max_length='15')
+    name = models.CharField(max_length=100)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    location = models.CharField(max_length='30')
+    location = models.CharField(max_length=30)
     description = models.TextField()
 
-    question_5 = models.CharField(max_length='50', blank=True)
-    question_6 = models.CharField(max_length='50', blank=True)
-    question_7 = models.CharField(max_length='50', blank=True)
-    question_8 = models.CharField(max_length='50', blank=True)
-    question_9 = models.CharField(max_length='50', blank=True)
+    question_5 = models.CharField(max_length=50, blank=True)
+    question_6 = models.CharField(max_length=50, blank=True)
+    question_7 = models.CharField(max_length=50, blank=True)
+    question_8 = models.CharField(max_length=50, blank=True)
+    question_9 = models.CharField(max_length=50, blank=True)
 
